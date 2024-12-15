a899539559b8cf78098daf29d71beaf1ca7d2e5b
Isabele Torres <isabeledasilvatorres@gmail.com>
2024-12-15 14:30:59 -0300

    Adicionando conteúdos de estudos

---
+++ EXERCICIOS-JS/Fundamentos/templateString.js
@@ -0,0 +1,14 @@
+const nome = 'Rebeca'
+
+const template = `
+    olá 
+    ${nome}!
+`
+console.log(template)
+
+// expressoes
+
+console.log(`1 + 1 = ${1+1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei.....${up('Cuidado')}!`)
