17ff774d022100adddd934d6295757fff71306d2
Isabele Torres <isabeledasilvatorres@gmail.com>
2024-12-15 15:13:50 -0300

    if

---
+++ EXERCICIOS-JS/Estrutura-de-controle/if1.js
@@ -0,0 +1,13 @@
+function soBoaNoticia(nota){
+    if (nota>=7){
+        console.log('Aprovada com '  + nota)
+    }
+}
+soBoaNoticia(8.1)
+
+function seForVerdadeEuFalo(valor){
+    if(valor){
+        console.log('É verdade...' + valor)
+    }
+}
+seForVerdadeEuFalo(' ')
