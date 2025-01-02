e8883bac5515ae93ab0e18fe1105e9a818d18528
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-01-02 08:35:47 -0300

    OOP

---
+++ EXERCICIOS-JS/OOP/oo.js
@@ -0,0 +1,24 @@
+//Código não executável
+
+//Procedural 
+
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento()
+
+//Princípios importantes:
+//1. Abristação
+//2. Encapsulamento
+//3. Herança
+//4. Polimofismo
